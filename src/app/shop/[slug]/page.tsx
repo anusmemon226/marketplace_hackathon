f5ee4afb2b9b6c22b3050ae4eeabb163db3ae458
anusmemon226 <anusm226@gmail.com>
2025-02-06 03:38:48 +0500

    Bugs Fixed

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import ProductDetail from '@/components/ProductDetail'
-const page = async ({ params }: { params: { slug: string } }) => {
+const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const { slug } = await params
     return (
         <ProductDetail slug={slug} />
